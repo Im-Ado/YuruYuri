05455b158abec4323ea401febc553a56633181ec
Ado <me.ado926codes@gmail.com>
2025-06-19 15:56:05 -0600

    Actualizar descarga playdoc.js

--- plugins/descarga playdoc.js
+++ plugins/descarga playdoc.js
@@ -1,37 +1,55 @@
 import fetch from 'node-fetch';
 
-let handler = async(m, { conn, usedPrefix, command, text }) => {
+const JT = {
+  contextInfo: {
+    externalAdReply: {
+      title: packname,
+      body: textbot,
+      mediaType: 1,
+      previewType: 0,
+      mediaUrl: null,
+      sourceUrl: null,
+      thumbnail: thumb,
+      renderLargerThumbnail: true,
+    },
+  },
+};
 
-if (!text) return m.reply(`✐ Ingresa Un Texto Para Buscar En Youtube\n> *Ejemplo:* ${usedPrefix + command}ozuna`);
+let handler = async (m, { conn, usedPrefix, command, text }) => {
 
-try {
-let api = await (await fetch(`https://delirius-apiofc.vercel.app/search/ytsearch?q=${text}`)).json();
+  if (!text) return m.reply(`✐ Ingresa Un Texto Para Buscar En Youtube\n> *Ejemplo:* ${usedPrefix + command} ozuna`);
 
-let results = api.data[0];
+  try {
+    let api = await (await fetch(`https://delirius-apiofc.vercel.app/search/ytsearch?q=${text}`)).json();
+    let results = api.data[0];
 
-let txt = `*「✦」 ${results.title}*
+    let txt = `*「✦」 ${results.title}*
 
-> ✦ *Canal* » ${results.author.name}\n> ⴵ *Duración:* » ${results.duration}\n> ✰ *Vistas:* » ${results.views}
-> ✐ *Publicación* » ${results.publishedAt}\n> 🜸 *Link* » ${results.url} `;
+> ✦ *Canal* » ${results.author.name}
+> ⴵ *Duración:* » ${results.duration}
+> ✰ *Vistas:* » ${results.views}
+> ✐ *Publicación* » ${results.publishedAt}
+> 🜸 *Link* » ${results.url}`;
 
-let img = results.image;
+    let img = results.image;
 
-conn.sendMessage(m.chat, { image: { url: img }, caption: txt }, { quoted: m });
+    // Aquí se aplica el JT al mensaje con imagen y caption
+    conn.sendMessage(m.chat, { image: { url: img }, caption: txt, ...JT }, { quoted: m });
 
-let api2 = await(await fetch(`https://api.vreden.my.id/api/ytmp3?url=${results.url}`)).json();
+    let api2 = await (await fetch(`https://api.vreden.my.id/api/ytmp3?url=${results.url}`)).json();
 
-await conn.sendMessage(m.chat, { 
-  audio: { url: api2.result.download.url }, 
-  mimetype: 'audio/mpeg', 
-  ptt: true // ← ahora se envía como nota de voz 
-}, { quoted: m });
+    await conn.sendMessage(m.chat, { 
+      audio: { url: api2.result.download.url }, 
+      mimetype: 'audio/mpeg', 
+      ptt: true 
+    }, { quoted: m });
 
-} catch (e) {
-m.reply(`Error: ${e.message}`);
-m.react('✖️');
+  } catch (e) {
+    m.reply(`Error: ${e.message}`);
+    m.react('✖️');
   }
-}
+};
 
 handler.command = ['play', 'pdoc'];
 
-export default handler
+export default handler;
