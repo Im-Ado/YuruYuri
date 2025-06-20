ff2ef96543f525ca784d529765e69191d2dba6c3
Ado <me.ado926codes@gmail.com>
2025-06-20 15:42:51 -0600

    Actualizar películas.js

--- plugins/películas.js
+++ plugins/películas.js
@@ -1,40 +1,49 @@
 //Creado por https://github.com/DIEGO-OFC/DORRAT-BOT-MD
+// Y editado por Ado 
+// github.com/Im-Ado
 
 import fetch from "node-fetch";
 
-let handler = async (m, {text, usedPrefix, command, conn}) => {
+let handler = async (m, { text, usedPrefix, command, conn }) => {
   if (!text) throw `*[❕] Ingrese el nombre de una película*\n\n*❍ EJEMPLO: ${usedPrefix + command} Batman*`;
 
-  let a = await fetch(`https://www.omdbapi.com/?t=${text}&apikey=caba8d6f`);
+  let a = await fetch(`https://www.omdbapi.com/?t=${encodeURIComponent(text)}&apikey=caba8d6f`);
   let x = await a.json();
 
-  // Verificar si hay un error en la respuesta de la API
   if (x.Response === "False") throw `*[❕] No se encontró la película: ${text}*`;
 
-  // Definiendo las variables 'comienzo' y 'fin'
-  const comienzo = "✧"; // Puedes cambiar esto por lo que desees
-  const fin = "✿"; // Puedes cambiar esto por lo que desees
+  const comienzo = "✦";
+  const fin = "✦";
 
-  let mov_txt = `*${comienzo}「 PELÍCULAS 」${fin}*\n
-*Titulo ∙* ${x.Title || "-"}
-*Publicado ∙* ${x.Year || "-"}
-*Duracion ∙* ${x.Runtime || "-"}
-*Genero ∙* ${x.Genre || "-"}
-*Director ∙* ${x.Director || "-"}
-*Actores ∙* ${x.Actors || "-"}
-*Lenguajes ∙* ${x.Language || "-"}
-*Awards ∙* ${x.Awards || "-"}
-*Votos ∙* ${x.imdbVotes || "-"}
-*Score ∙* ${x.Metascore || "-"}
-*Tipo ∙* ${x.Type || "-"}
-*Recaudado ∙* ${x.BoxOffice || "-"}
-*Grabacion ∙* ${x.Country || "-"}
+  let mov_txt = `
+${comienzo} 𝗣 𝗘 𝗟 Í 𝗖 𝗨 𝗟 𝗔 𝗦 ${fin}
+•━━━━━━━━━━━━━•
+> 🎬 *Título:* ${x.Title || "-"}
+> 📆 *Publicado:* ${x.Year || "-"}
+> ⏱ *Duración:* ${x.Runtime || "-"}
+> 🎭 *Género:* ${x.Genre || "-"}
+> 🎥 *Director:* ${x.Director || "-"}
+> 👥 *Actores:* ${x.Actors || "-"}
+> 🗣 *Lenguajes:* ${x.Language || "-"}
+> 🏆 *Premios:* ${x.Awards || "-"}
+> ⭐ *Votos IMDb:* ${x.imdbVotes || "-"}
+> 💯 *Score Metascore:* ${x.Metascore || "-"}
+> 🎞 *Tipo:* ${x.Type || "-"}
+> 💰 *Recaudado:* ${x.BoxOffice || "-"}
+> 🌍 *País:* ${x.Country || "-"}
+•━━━━━━━━━━━━━•
+🤍 Pedido por @${m.sender.split("@")[0]}
+*Yuru Yuri Bot*`;
 
-   ❕Pedido por @${m.sender.split("@")[0]}
-*Yuri Bot*`;
-
-  // Enviando el mensaje
-  await conn.sendMessage(m.chat, {image: {url: x.Poster}, caption: mov_txt, mentions: [m.sender]}, {quoted: m});
+  await conn.sendMessage(
+    m.chat,
+    {
+      image: { url: x.Poster || 'https://files.catbox.moe/xr2m6u.jpg' },
+      caption: mov_txt.trim(),
+      mentions: [m.sender],
+    },
+    { quoted: m }
+  );
 };
 
 handler.command = ['pelicula', 'peli'];
