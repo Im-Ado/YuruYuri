677ca5ee70643d6016067cfb0ead296fd82d005d
Ado <me.ado926codes@gmail.com>
2025-06-21 10:55:24 -0600

    Actualizar buscador-pinterest.js

--- plugins/buscador-pinterest.js
+++ plugins/buscador-pinterest.js
@@ -1,48 +1,25 @@
 import axios from 'axios'
-import baileys from '@whiskeysockets/baileys'
 
-let handler = async (m, { conn, text }) => {
-  if (!text) return m.reply(`❀ Por favor, ingresa lo que deseas buscar por Pinterest.`)
+let HS = async (m, { conn, text }) => {
+if (!text) return conn.reply(m.chat, `✎ Ingresa un texto para buscar en pinterest`, m)
 
-  try {
-    m.react('🕒')
-    let results = await pins(text)
+try {
+let api = await axios.get(`https://api.siputzx.my.id/api/s/pinterest?query=${text}`)
+let json = api.data
+let data = json.data[Math.floor(Math.random() * json.data.length)]
 
-    if (!results.length) return conn.reply(m.chat, `✧ No se encontraron resultados para "${text}".`, m)
+let { pin, created_at, images_url, grid_title } = data
+let HS = `*「✦」 ${grid_title}*
 
-    const medias = results.slice(0, 10).map(img => ({ type: 'image', data: { url: img.hd } }))
+> *✦ Creador: » ${created_at}*
+> *🜸 Link: » ${pin}*`
+await conn.sendMessage(m.chat, { image: { url: images_url }, caption: HS }, { quoted: m })
+//await conn.sendMessage(m.chat, { image: images_url, caption: HS, footer: '', buttons: [ { buttonId: `.pinterest ${text}`, buttonText: { displayText: 'Siguiente' } }, ], viewOnce: true, headerType: 4 }, { quoted: m })
 
-    await conn.sendMessage(m.chat, medias, {
-      caption: `❀  Pinterest  -  Search  ❀\n\n✧ Búsqueda » "${text}"\n✐ Resultados » ${medias.length}\n\n${dev}`,
-      quoted: m
-    })
+} catch (error) {
+console.error(error)
+}}
 
-    await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key } })
-  } catch (error) {
-    conn.reply(m.chat, `⚠︎ Error:\n\n${error.message}`, m)
-  }
-}
+HS.command = ['pinterest', 'pinterestsearch']
 
-handler.help = ['pinterest']
-handler.command = ['pinterest', 'pin']
-handler.tags = ['dl']
-
-export default handler
-
-const pins = async (query) => {
-  try {
-    const { data } = await axios.get(`https://api.stellarwa.xyz/search/pinterest?query=${query}`)
-
-    if (data?.status && data?.data?.length) {
-      return data.data.map(item => ({
-        hd: item.hd,
-        mini: item.mini
-      }))
-    }
-
-    return []
-  } catch (error) {
-    console.error("Error al obtener imágenes de Pinterest:", error)
-    return []
-  }
-}
+export default HS
