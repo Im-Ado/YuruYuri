99fb3a66cb455266479ebab63eb3de5564a86cc1
Ado <me.ado926codes@gmail.com>
2025-06-20 20:41:37 -0600

    Crear _lid.js

---
+++ plugins/_lid.js
@@ -0,0 +1,11 @@
+const handler = async (m, { conn }) => {
+  const sender = m.sender; // esto ya te da el jid completo: 521234567890@s.whatsapp.net
+  const number = sender.split('@')[0]; // aquí extraes solo el número
+
+  await conn.reply(m.chat, `🧩 *Número detectado:*\n➤ ${number}`, m);
+};
+
+handler.command = ["lid", "getid"];
+handler.tags = ["tools"];
+handler.help = ["lid"];
+export default handler;
