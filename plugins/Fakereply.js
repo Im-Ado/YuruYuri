527fa5982e803a2057d635eae79135c4f160ee15
Ado <me.ado926codes@gmail.com>
2025-06-24 14:38:34 -0600

    Actualizar Fakereply.js

--- plugins/Fakereply.js
+++ plugins/Fakereply.js
@@ -3,6 +3,8 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, args }) => {
   if (!args[0]) return m.reply('🎵 Pasa el link del video de YouTube')
 
+  await m.react('🎶') // reacción de espera
+
   const res = await fetch(`https://api.siputzx.my.id/api/d/ytmp3?url=${encodeURIComponent(args[0])}`)
   const json = await res.json()
 
@@ -13,19 +15,11 @@ let handler = async (m, { conn, args }) => {
   await conn.sendMessage(m.chat, {
     audio: { url },
     mimetype: 'audio/mpeg',
-    ptt: true, // si querés que se escuche como nota de voz
-    fileName: `${title}.mp3`,
-    contextInfo: {
-      externalAdReply: {
-        title: title,
-        body: "Shadow Ultra Edited 💿",
-        thumbnailUrl: null, // o pon una tuya
-        mediaType: 2,
-        mediaUrl: args[0],
-        sourceUrl: args[0]
-      }
-    }
+    ptt: true,
+    fileName: `${title}.mp3`
   }, { quoted: m })
+
+  await m.react('✅') // reacción final
 }
 
 handler.command = ['play3']
